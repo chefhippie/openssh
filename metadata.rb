cb00e8b1b0d36993fe9e409924de57ccc2bbb3fb
Thomas Boerger <tboerger@tbpro.de>
2013-10-18 16:03:53 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "openssh"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures openssh"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "openssh", "Installs/Configures openssh"
+
+supports "suse"
